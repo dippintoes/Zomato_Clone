0a87d5bade370836f00ed55fd1ef0196b5563008
Rutuja Gosavi <gosavirutu523@gmail.com>
2021-10-07 15:39:56 +0530

    done with setup, configurations and models

---
+++ server/database/food/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const FoodSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    isVeg: {type: Boolean, required: true},
+    containsEggs:{type: Boolean, required: true},
+    category: {type: String, required: true},
+    photos: {
+        type: mongoose.Types.ObjectId, ref: "Images",
+    },//Which type of it is and which collection to refer.
+    price:{type: String,default: 150 ,required: true},//because it cant have zero price
+    addOns: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Foods",
+    }],
+    restaurant: {
+        type: mongoose.Types.ObjectId,
+        ref: "Restaurants", required: true,
+    },
+});
+
+export const FoodModel = mongoose.model("Foods",FoodSchema);
